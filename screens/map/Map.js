088257134806097711f3189d52818d55e25ae51f
MartaFradique <martafradique@ua.pt>
2023-04-03 00:21:09 +0100

    test my ssh key

--- screens/map/Map.js
+++ screens/map/Map.js
@@ -1,9 +1,35 @@
-import React from 'react'
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import * as Mapbox from 'mapbox-gl';
 
-const map = () => {
+
+
+
+Mapbox.setAccessToken('pk.eyJ1IjoibWFydGlzc2UiLCJhIjoiY2xmdjlvNWJrMDJhbjNocHZoZGRlejMzaCJ9.BBAqo1VGk-00bSPlJwCoxw');
+
+const Map = () => {
   return (
-    <div>map</div>
-  )
+    <View style={styles.page}>
+    <View style={styles.container}>
+      <Mapbox.MapView style={styles.map} />
+    </View>
+  </View>
+  );
 }
 
-export default map
+export default Map;
+
+const styles = StyleSheet.create({
+  page: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  container: {
+    height: 300,
+    width: 300,
+  },
+  map: {
+    flex: 1
+  }
+});
