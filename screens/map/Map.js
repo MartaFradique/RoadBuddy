729b6ab0dbf13ba8d2d8b3c792dda6599dc37603
TheScorpoi <sobral@ua.pt>
2023-04-05 21:43:30 +0100

    tentar map

--- screens/map/Map.js
+++ screens/map/Map.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, {  useState } from "react";
+import { View, StyleSheet, Text,  Image } from "react-native";
+import MapboxGL from "@react-native-mapbox-gl/maps";
 
-const map = () => {
+MapboxGL.setAccessToken("pk.eyJ1IjoibWFydGlzc2UiLCJhIjoiY2xmdjlvNWJrMDJhbjNocHZoZGRlejMzaCJ9.BBAqo1VGk-00bSPlJwCoxw");
+
+const Map = () => {
+  const [coordinates] = useState([8.674252499999994, 9.0845755]);
   return (
-    <div>map</div>
-  )
+    <View style={styles.page}>
+      <View style={styles.container}>
+        <MapboxGL.MapView style={styles.map}>
+          <MapboxGL.Camera zoomLevel={6}
+           centerCoordinate={coordinates} />
+          <MapboxGL.PointAnnotation coordinate={coordinates} />
+         </MapboxGL.MapView>
+         </View>
+       </View>
+  );
 }
-
-export default map
+const styles = StyleSheet.create({
+  page: {
+    flex: 1,
+  },
+  container: {
+    height: "100%",
+    width: "100%",
+    flex: 1,
+  },
+  map: {
+    flex: 1,
+  }
+});
+export default Map;
